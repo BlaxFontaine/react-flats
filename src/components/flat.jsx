94ac7e0f9d7339df8bd00ac6fcef9b7a8cd55d75
Blax Fontaine <valerie.gehle@gmail.com>
2019-10-03 13:58:18 +0200

    flats style ok

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -3,14 +3,17 @@ import React, { Component } from 'react';
 class Flat extends Component {
 
   render() {
+    const cardstyle = {
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${this.props.imageUrl})`
+    };
 
     return (
-      <div className="card" >
-        <div className="card-category">`${this.props.price} ${this.props.priceCurrency}`</div>
+      <div className="card" style={cardstyle} >
+        <div className="card-category">{this.props.price} {this.props.priceCurrency}</div>
         <div className="card-description">
-          <h2>`${this.props.name}`</h2>
+          <h2>{this.props.name}</h2>
         </div>
-        <a className="card-link"></a>
+        <a className="card-link" href={"#"}></a>
       </div>
       );
    }
